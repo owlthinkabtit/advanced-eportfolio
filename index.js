6670e9e80ad3548339c8a0b23de3fedc0f6d64e5
owlthinkabtit <haggins.jameka@gmail.com>
2024-06-26 15:32:47 -0400

    working on the js portion of this code now

---
+++ index.js
@@ -0,0 +1,19 @@
+// template_hwc0sls
+// service_ixamp5k
+// 0wlUWQFSvr417uUTg
+
+function contact(event) {
+    event.preventDeafult();
+    emailjs
+    //.sendForm(
+      //'service_ixamp5k',
+      //'template_hwc0sls',
+      //event.target,
+      //'0wlUWQFSvr417uUTg'//
+    //).then(() => {
+    // console.log('this worked') 
+    //})
+    setTimeout(() => {
+      console.log('it worked 1')
+    }, 500);
+}
